9aa5b7960b3cb1333aacaecb4599656bd23b2951
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2021-02-07 04:06:42 +0900

    no message

---
+++ lesson3/season.ts
@@ -0,0 +1,9 @@
+enum SEASONS{SPRING, SUMMER, AUTUMN, WINTER};
+var season:SEASONS;
+season = SEASONS.SUMMER;
+
+if(season == SEASONS.SUMMER){
+    console.log("夏ですね");
+} else {
+    console.log("夏ではないですね");
+}
